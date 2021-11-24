c108fdbe8bc2ae9cb1033115aba50bfff1e9e1dc
unknown <mg-milan@hotmail.rs>
2021-11-24 18:55:01 +0100

    adding components

---
+++ src/components/Card/card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card.modules.css";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h2>Hello from Card</h2>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Hero/hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./hero.modules.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h2>Hello from Hero</h2>
+    </div>
+  );
+};
+
+export default Hero;
